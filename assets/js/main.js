934d4caa1ca71603dae168f9bf120fdd0a70f41a
KENLLY CHACON <86888726+Kenlly-Chacon@users.noreply.github.com>
2021-10-11 22:50:17 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,9 +248,9 @@
     strings: ['<class="soy">Programador','<class="soy">Desarrollador Web'],
 
     //stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-    typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+    typeSpeed: 70, // Velocidad en mlisegundos para poner una letra,
     startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-    backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+    backSpeed: 70, // Velocidad en milisegundos para borrrar una letra,
     //smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
     shuffle: false, // Alterar el orden en el que escribe las palabras.
     backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
